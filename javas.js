ffbcb1e6067a6f9c1eb363a626e285da8507be2b
gowrishankar <gowrishankarselvarj27@gmail.com>
2023-08-03 15:10:27 +0530

    index.html

---
+++ javas.js
@@ -0,0 +1,39 @@
+var a = 8
+console.log(a)
+
+{
+    let b =30
+    console.log(b)
+}
+
+const c = 23
+console.log(c)
+
+var a = 0
+if(a == 0){
+    console.log("It is Zero")
+}
+
+else if(a > 0){
+        console.log("The number is positive")
+    }
+
+    
+else{
+        console.log("The number is negative")
+    }
+
+a=6
+i = 0
+while(i <= a){
+    console.log(i)
+    i++
+}
+
+a=3
+for(i=0; i<a; i++){
+    console.log(i)
+}
+
+var array = [2,4,8,16,32,64,128,256,512,1024,2048]
+array.forEach(element => console.log(element))
